5757385c480fd2378f3c1acd58b486b6a40ac01b
philip martinez <martinez.philip00@gmail.com>
2022-05-09 22:39:07 -0500

    commit tree d33d20d2aabd34eb15ced3bca4df25ffde2b8f8d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Philip',
+    streetAddress: '14 Broadway'
+};
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newEmployee = { ...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+// `destructivelyUpdateEmployeeWithKeyAndValue()`: this function should work the
+//same as `updateEmployeeWithKeyAndValue()` but it _should_ mutate the
+//`employee` `Object` passed in.
+
+function deleteFromEmployeeByKey (employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee [key] = value;
+    return newEmployee;
+}
+
+delete deleteFromEmployeeByKey.key;
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
